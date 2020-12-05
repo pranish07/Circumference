ffa7b1b7e929c0317f52587cfd9e581eae45c2f7
pranish07 <49307206+pranish07@users.noreply.github.com>
2020-12-05 23:17:09 +0545

    Add files via upload

---
+++ circumference.js
@@ -0,0 +1,7 @@
+function calcCircumference(){
+    var radius = 2; 
+    var pi = 3.141592654;
+    var circumference = 2 * pi * radius ;
+    alert("The circumference is " + circumference);
+    }
+    
